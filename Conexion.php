622eecf8c415557dc37e47efc62066e81e56b8b9
PabloBarriosALADI <pbarrios@aladi.org>
2025-06-20 11:43:25 -0300

    modified:   Conexion.php 	new file:   Resources/ActionInsert.gif 	new file:   Resources/DeleteRow.gif 	new file:   estilos/PHPTabla-001.css 	new file:   estilos/PHPTabla.css 	new file:   quinto

--- Conexion.php
+++ Conexion.php
@@ -3,8 +3,8 @@ $servername = "localhost:3306";
 $username = "root";
 $password = "Aladi1980+";
 $dbname = "consultaweb";
-
-try {
+//primer segundo
+// try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
